dee72e31cac7296f122eec279102b813a308dee8
mitchellmarkoff <mitchell.markoff@pantheon.io>
2022-12-05 14:26:35 -0500

    test

--- next.config.js
+++ next.config.js
@@ -18,19 +18,35 @@ if (
 	}
 	throw new Error(message);
 }
-
+let PANTHEON_ENVIRONMENT_PREFIX
+console.log('CHECKING ENV 2')
 if (process.env.PANTHEON_ENVIRONMENT_URL) {
-	let PANTHEON_ENVIRONMENT_PREFIX = undefined
+	console.log(process.env.PANTHEON_ENVIRONMENT_URL)
+	// let PANTHEON_ENVIRONMENT_PREFIX = undefined
 	let IS_LIVE_ENVIRONMENT = undefined
 	const envPrefix =
 		process.env.PANTHEON_ENVIRONMENT_URL.match(/^([^-]*)/)[0];
-	if (envPrefix === 'live') {
+	console.log('Env prefix')
+	console.log(envPrefix)
+	console.log('Checking')
+	PANTHEON_ENVIRONMENT_PREFIX = process.env.PANTHEON_ENVIRONMENT_URL.match(/^([^-]*-)[^-]*/)[0];
+	if (envPrefix !== 'live') {
+		console.log('IS NOT LIVE')
+		PANTHEON_ENVIRONMENT_PREFIX = process.env.PANTHEON_ENVIRONMENT_URL.match(/^([^-]*-)[^-]*/)[0];
+		console.log('Test match')
+		const match = process.env.PANTHEON_ENVIRONMENT_URL.match(/^([^-]*-)[^-]*/)[0];
+		console.log(match)
+	} else {
 		PANTHEON_ENVIRONMENT_PREFIX = 'live'
 		IS_LIVE_ENVIRONMENT = 'live'
-	} else {
-		PANTHEON_ENVIRONMENT_PREFIX = process.env.PANTHEON_ENVIRONMENT_URL.match(/^([^-]*-)[^-]*/)[0];
 	}
 }
+
+console.log('Done')
+console.log(PANTHEON_ENVIRONMENT_PREFIX)
+console.log(match)
+console.log('finished')
+
 
 let backendUrl, imageDomain;
 if (process.env.WPGRAPHQL_URL === undefined) {
